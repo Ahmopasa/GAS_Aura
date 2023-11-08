4b59c7bd383203aeca506a19f88c69cb95935532
Ahmet Kandemir PEHLİVANLI <ahmet.pehlivanli@yahoo.com>
2023-11-08 20:12:43 +0300

    Player and Enemy classes have been added.
    The two classed has been inherited from the base character class. Also, the folder structure of these classes have been reorganized. Additionally, some features has been removed from the base character class since these will be implemented in their respective classes.

--- Source/Aura/Public/Character/AueaCharacterBase.h
+++ Source/Aura/Public/Character/AueaCharacterBase.h
@@ -6,24 +6,15 @@
 #include "GameFramework/Character.h"
 #include "AueaCharacterBase.generated.h"
 
-UCLASS()
+UCLASS(Abstract)
 class AURA_API AAueaCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AAueaCharacterBase();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
---
+++ Source/Aura/Public/Character/AueaEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AueaCharacterBase.h"
+#include "AueaEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAueaEnemy : public AAueaCharacterBase
+{
+	GENERATED_BODY()
+	
+};
